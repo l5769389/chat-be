359eac4da9717ba3fb6747750370be552ef21146
l5769389 <z31014047@163.com>
2023-05-20 16:20:29 +0800

    no message

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
-
+import { readFileSync } from 'fs';
+import { join, resolve } from 'path';
+console.log(__dirname);
 const {
   http: { port },
 } = configuration();
-console.log(port);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: readFileSync(join(__dirname, '/config/rootCA.pem')),
+      cert: readFileSync(join(__dirname, '/config/rootCA-key.pem')),
+    },
+  });
   app.enableCors();
   await app.listen(port);
 }
